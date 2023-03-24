daa6c3e31a35049d156071035b5ef31e2d8ae1ec
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-03-24 11:46:21 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+stopBtn.disabled = true;
+
+startBtn.addEventListener("click", changeColor);
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    stopBtn.disabled = false;
+    startBtn.disabled = true;
+  }, 1000);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
