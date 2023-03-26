e8ef40fab46b8b69de9874b93e13330d9f27725f
AleksanderMukhin <116069326+AleksanderMukhin@users.noreply.github.com>
2023-03-26 10:25:40 +0000

    Deploying to gh-pages from @ AleksanderMukhin/goit-js-hw-09@febed704fa08bc9b57db83a7cdf656e3064065d3 🚀

---
+++ 01-color-switcher.e4934dcb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");function n(){return"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}d.disabled=!0,e.addEventListener("click",(function(){d.disabled=!1,e.disabled=!0,t.style.backgroundColor=n(),timerId=setInterval((function(){t.style.backgroundColor=n()}),1e3)})),d.addEventListener("click",(function(){clearInterval(timerId),e.disabled=!1,d.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.e4934dcb.js.map
